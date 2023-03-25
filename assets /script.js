ede6936dd21d89c0a73d435bf2186811027bbcda
Katharine McClenaghan <kemcclen@gmail.com>
2023-03-25 15:41:48 -0400

    Questions and answers added in JS, Intro and question answer selectors added in HTML

---
+++ assets /script.js
@@ -0,0 +1,31 @@
+var questionsArray = [
+    {
+        question: "What tag is used to wrap the JavasScript code in the HTML? ",
+        choices: ["1. <a> </a>", "2. <link> </link>", "3. <script> </script>", "4. <java> </java>"],
+        answer: 2
+    },
+
+    {
+        question: "What one is NOT a primitive or basic datatype of JavaScript?",
+        choices: ["1. Python", "2. Strings", "3. Booleons", "4. Null"],
+        answer: 0
+    },
+
+    {
+        question: "What keyword is used to tell JavaScript you are declaring a variable?",
+        choices: ["1. vrbl=", "2. vr:", "3. variable= ", "4. var "],
+        answer:3
+    },
+
+    {
+        question: "How can you differentiate a string and a number variable if they both include numerical values?",
+        choices: ["1. They are interchangeable", "2. Quotation marks indicate a string", "3. the number is wirrten like twenty two", "4. You can't" ],
+        answer:1
+    },
+
+    {
+        question: "What number do you start with when counting values in JavaScript?",
+        choices: ["1. 0", "2. 1", "3. You count by 2's", "4. 0.1 "],
+        answer:0
+    },
+];
